84d3dec7d147f3c1e21c2bc06de7ea7740a47b1d
Bruno Polido <brunopolidoandrade@gmail.com>
2022-12-01 20:24:03 -0300

    Xadrez

---
+++ saidaDeDados/src/casting.java
@@ -0,0 +1,18 @@
+
+
+public class casting {
+	public static void main(String[] args) {
+		
+		int a, b;
+		
+		a = 5 ;
+		b = 2 ; 
+		double resultado;
+		
+		resultado = /*casting*/(double)/*casting*/ a / b;
+		
+		System.out.println(resultado);
+		
+	}
+
+}
